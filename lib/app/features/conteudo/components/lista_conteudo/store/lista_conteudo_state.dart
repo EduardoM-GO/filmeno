2fd5a97e6278b37a59425ffd5f314a4ef639505c
Eduardo M <eduardommlima@outlook.com>
2024-02-20 21:27:20 -0300

    Feat: Removido componentes e store de FilmesEmCartaz e adicionado componentes e store de ListaConteudo

---
+++ lib/app/features/conteudo/components/lista_conteudo/store/lista_conteudo_state.dart
@@ -0,0 +1,30 @@
+part of 'lista_conteudo_store.dart';
+
+sealed class ListaConteudoState extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+final class ListaConteudoInicialState extends ListaConteudoState {}
+
+final class ListaConteudoCarregandoState extends ListaConteudoState {}
+
+final class ListaConteudoSucessoState extends ListaConteudoState {
+  final List<Conteudo> conteudos;
+
+  ListaConteudoSucessoState(this.conteudos);
+
+  @override
+  List<Object?> get props => [conteudos];
+}
+
+final class ListaConteudoFalhaState extends ListaConteudoState {}
+
+final class ListaConteudoTodosConteudosCarregadosState
+    extends ListaConteudoState {
+  final List<Conteudo> conteudos;
+
+  ListaConteudoTodosConteudosCarregadosState(this.conteudos);
+  @override
+  List<Object?> get props => [conteudos];
+}
---
+++ lib/app/features/conteudo/components/lista_conteudo/store/lista_conteudo_store.dart
@@ -0,0 +1,52 @@
+import 'package:equatable/equatable.dart';
+import 'package:filmeno/app/features/conteudo/domain/entities/conteudo.dart';
+import 'package:filmeno/app/shared/domain/entities/metadados.dart';
+import 'package:filmeno/app/shared/domain/entities/resultado_com_metadados.dart';
+import 'package:filmeno/app/shared/falha/falha.dart';
+import 'package:filmeno/app/shared/store/store.dart';
+import 'package:result_dart/result_dart.dart';
+
+part 'lista_conteudo_state.dart';
+
+typedef ListaConteudoUseCase
+    = Future<Result<ResultadoComMetadados<List<Conteudo>>, Falha>> Function(
+        {int? proximaPagina});
+
+class ListaConteudoStore extends Store<ListaConteudoState> {
+  final ListaConteudoUseCase _useCase;
+
+  ListaConteudoStore(this._useCase) : super(ListaConteudoInicialState());
+
+  final List<Conteudo> _conteudos = [];
+
+  int? _proximaPagina;
+  bool _todosConteudosCarregados = false;
+
+  Future<void> buscarConteudos() async {
+    if (_todosConteudosCarregados) {
+      emit(ListaConteudoTodosConteudosCarregadosState(_conteudos));
+      return;
+    }
+
+    emit(ListaConteudoCarregandoState());
+    final result = await _useCase(proximaPagina: _proximaPagina);
+
+    emit(result.fold(
+      (success) {
+        final Metadados metadados = success.metadados;
+        _proximaPagina = metadados.proximaPagina;
+        _todosConteudosCarregados = !metadados.temProximaPagina;
+
+        final List<Conteudo> conteudos = success.resultado;
+        _conteudos.addAll(conteudos);
+
+        if (_todosConteudosCarregados) {
+          return ListaConteudoTodosConteudosCarregadosState(_conteudos);
+        }
+
+        return ListaConteudoSucessoState(_conteudos);
+      },
+      (failure) => ListaConteudoFalhaState(),
+    ));
+  }
+}
