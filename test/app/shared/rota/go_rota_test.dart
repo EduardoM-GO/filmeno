6969f16c04cba3e9e804299cdf8ab181e0c258cd
Eduardo M <eduardommlima@outlook.com>
2024-02-27 22:11:52 -0300

    Feat: alterado nome da pasta de modular para rota

--- test/app/shared/rota/go_rota_test.dart
+++ test/app/shared/rota/go_rota_test.dart
@@ -1,4 +1,4 @@
-import 'package:filmeno/app/shared/modular/go_rota.dart';
+import 'package:filmeno/app/shared/rota/go_rota.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get_it/get_it.dart';
